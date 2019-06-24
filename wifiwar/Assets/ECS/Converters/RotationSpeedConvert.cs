bd2001363c9715f29354b5586bc8955111423b6d
Ryan Jones <ryanjo@RYANJONES-MBP.local>
2019-06-24 20:46:50 +0200

    inital commit of rotating cube.

---
+++ wifiwar/Assets/ECS/Converters/RotationSpeedConvert.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class RotationSpeedConvert : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float rotationSpeed = 45.0f;
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var data = new RotationSpeedComponent() { rotationSpeed = math.radians(rotationSpeed) };
+        dstManager.AddComponentData(entity, data);
+    }
+}
---
+++ wifiwar/Assets/ECS/Systems/RotationSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class RotationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        /*
+         *         Entities.ForEach((ref RotationSpeed_ForEach rotationSpeed, ref Rotation rotation) =>
+        {
+            var deltaTime = Time.deltaTime;
+            rotation.Value = math.mul(math.normalize(rotation.Value),
+                quaternion.AxisAngle(math.up(), rotationSpeed.RadiansPerSecond * deltaTime));
+        });
+         */
+        Entities.ForEach((ref RotationSpeedComponent rotationSpeed, ref Rotation rotation) =>
+        {
+            float deltaTime = Time.deltaTime;
+            rotation.Value = math.mul(math.normalize(rotation.Value),
+                Quaternion.AngleAxis(rotationSpeed.rotationSpeed * deltaTime, math.up()));
+        });
+    }
+}
