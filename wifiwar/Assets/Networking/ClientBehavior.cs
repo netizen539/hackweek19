262a214a742f7c92d5552e85cfa7f5aa5ad54eb1
Ryan Jones <ryanjo@RYANJONES-MBP.local>
2019-06-24 22:45:45 +0200

    Added simple client/server code and a network test scene.

---
+++ wifiwar/Assets/Networking/ClientBehavior.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Networking.Transport;
+
+using UdpCNetworkDriver = Unity.Networking.Transport.GenericNetworkDriver<Unity.Networking.Transport.IPv4UDPSocket, 
+    Unity.Networking.Transport.DefaultPipelineStageCollection>;
+
+struct ClientUpdateJob : IJob
+{
+    public UdpCNetworkDriver driver;
+    public NativeArray<NetworkConnection> connection;
+    public NativeArray<byte> done;
+
+    public void Execute()
+    {
+        if (!connection[0].IsCreated)
+        {
+            if (done[0] != 1)
+                Debug.Log("Something went wrong during connect");
+            return;
+        }
+		
+        DataStreamReader stream;
+        NetworkEvent.Type cmd;
+		
+        while ((cmd = connection[0].PopEvent(driver, out stream)) != 
+               NetworkEvent.Type.Empty)
+        {
+            switch (cmd)
+            {
+                case NetworkEvent.Type.Connect:
+                    Debug.Log("CLIENT: Our client is now connected to the server");
+                    break;
+                case NetworkEvent.Type.Data:
+                    Debug.Log("CLIENT: Got Back data from the server.");
+                    break;
+                case NetworkEvent.Type.Disconnect:
+                    Debug.Log("CLIENT: Our client was disconnected from the server");
+                    connection[0] = default(NetworkConnection);
+                    break;
+                
+            }
+        }
+    }
+}
+
+public class ClientBehavior : MonoBehaviour
+{
+    public UdpCNetworkDriver m_Driver;
+    public NativeArray<NetworkConnection> m_Connection;
+    public NativeArray<byte> m_Done;
+    
+    public JobHandle ClientJobHandle;
+
+    public string serverAddress;
+    public ushort serverPort;
+    
+    void Start ()
+    {
+        m_Driver = new UdpCNetworkDriver(new INetworkParameter[0]);
+        
+        m_Connection = new NativeArray<NetworkConnection>(1, Allocator.Persistent);
+        m_Done = new NativeArray<byte>(1, Allocator.Persistent);
+        NetworkEndPoint ep = new NetworkEndPoint();
+        
+        var endpoint = NetworkEndPoint.Parse(serverAddress, serverPort);
+        Debug.Log("CLIENT: Connecting to server at "+serverAddress);
+        m_Connection[0] = m_Driver.Connect(endpoint);
+    }
+    
+    public void OnDestroy()
+    {
+        ClientJobHandle.Complete();
+        m_Connection.Dispose();
+        m_Driver.Dispose();
+        m_Done.Dispose();
+    }
+	
+    void Update()
+    {
+        ClientJobHandle.Complete();
+        var job = new ClientUpdateJob
+        {
+            driver = m_Driver,
+            connection = m_Connection,
+            done = m_Done
+        };
+        ClientJobHandle = m_Driver.ScheduleUpdate();
+        ClientJobHandle = job.Schedule(ClientJobHandle);
+    }
+}
---
+++ wifiwar/Assets/Networking/ServerBehavior.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using Unity.Networking.Transport;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine.Assertions;
+using UdpCNetworkDriver = Unity.Networking.Transport.GenericNetworkDriver<Unity.Networking.Transport.IPv4UDPSocket, 
+    Unity.Networking.Transport.DefaultPipelineStageCollection>;
+
+
+struct ServerUpdateConnectionsJob : IJob
+{
+    public UdpCNetworkDriver driver;
+    public NativeList<NetworkConnection> connections;
+
+    public void Execute()
+    {
+        // CleanUpConnections
+        for (int i = 0; i < connections.Length; i++)
+        {
+            if (!connections[i].IsCreated)
+            {
+                connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+        // AcceptNewConnections
+        NetworkConnection c;
+        while ((c = driver.Accept()) != default(NetworkConnection))
+        {
+            connections.Add(c);
+            Debug.Log("SERVER: Accepted a connection");
+        }
+    }
+}
+
+struct ServerUpdateJob : IJobParallelForDefer
+{
+    public UdpCNetworkDriver.Concurrent driver;
+    public NativeArray<NetworkConnection> connections;
+
+    public void Execute(int index)
+    {
+        DataStreamReader stream;
+        if (!connections[index].IsCreated)
+            Assert.IsTrue(true);
+
+        NetworkEvent.Type cmd;
+        while ((cmd = driver.PopEventForConnection(connections[index], out stream)) !=
+               NetworkEvent.Type.Empty)
+        {
+            switch (cmd)
+            {
+                case NetworkEvent.Type.Connect:
+                    Debug.Log("SERVER: Client Connection detected");
+                    break;
+                case NetworkEvent.Type.Data:
+                    Debug.Log("SERVER: Client data detected");
+                    break;
+                case NetworkEvent.Type.Disconnect:
+                    Debug.Log("SERVER: Client Disconnected");
+                    connections[index] = default(NetworkConnection);
+                    break;
+                case NetworkEvent.Type.Empty:
+                    // Nothing to do. 
+                    break;
+                
+                    
+            }
+        }
+    }
+}
+
+public class ServerBehavior : MonoBehaviour
+{
+    public UdpCNetworkDriver m_Driver;
+    public NativeList<NetworkConnection> m_Connections;
+    private JobHandle ServerJobHandle;
+
+    public ushort listenPort;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
+        m_Driver = new UdpCNetworkDriver(new INetworkParameter[0]);
+        if (m_Driver.Bind(NetworkEndPoint.Parse(IPAddress.Any.ToString(), listenPort)) != 0)
+            Debug.Log("SERVER: Failed to bind to port 9000");
+        else
+            m_Driver.Listen();
+    }
+    
+    public void OnDestroy()
+    {
+        // Make sure we run our jobs to completion before exiting.
+        ServerJobHandle.Complete();
+        m_Connections.Dispose();
+        m_Driver.Dispose();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ServerJobHandle.Complete();
+        
+        var connectionJob = new ServerUpdateConnectionsJob
+        {
+            driver = m_Driver, 
+            connections = m_Connections
+        };
+        
+        var serverUpdateJob = new ServerUpdateJob
+        {
+            driver = m_Driver.ToConcurrent(),
+            connections = m_Connections.AsDeferredJobArray()
+        };
+        
+        ServerJobHandle = m_Driver.ScheduleUpdate();
+        ServerJobHandle = connectionJob.Schedule(ServerJobHandle);
+        ServerJobHandle = serverUpdateJob.Schedule(m_Connections, 1, ServerJobHandle);        
+    }
+}
