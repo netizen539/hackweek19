673e64548c2e575b9857c99e4f5018e886170200
Aurimas Cernius <aurimasc@unity3d.com>
2019-06-25 12:17:01 +0200

    Add movement component and system

---
+++ wifiwar/Assets/ECS/Components/MovementComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public enum MovementDirection
+{
+    UP = 0,
+    UP_RIGHT = 1,
+    RIGHT = 2,
+    DOWN_RIGHT = 3,
+    DOWN = 4,
+    DOWN_LEFT = 5,
+    LEFT = 6,
+    UP_LEFT = 7,
+}
+
+[Serializable]
+public struct MovementComponent : IComponentData
+{
+    public MovementDirection direction;
+    public float speed;
+}
---
+++ wifiwar/Assets/ECS/Systems/MovementSystem.cs
@@ -0,0 +1,61 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class MovementSystem : JobComponentSystem
+{
+    public static readonly float2[] directions = new float2[] {
+        new float2(0, 1),
+        new float2(1, 1),
+        new float2(1, 0),
+        new float2(1, -1),
+        new float2(0, -1),
+        new float2(-1, -1),
+        new float2(-1, 0),
+        new float2(-1, 1), 
+    };
+
+    [BurstCompile]
+    struct MovementSystemJob : IJobForEach<Translation, Rotation, MovementComponent>
+    {
+        public float deltaTime;
+        
+        
+        
+        public void Execute(ref Translation translation, [ReadOnly] ref Rotation rotation, [ReadOnly] ref MovementComponent movement)
+        {
+            float movementSpeed = movement.speed * deltaTime;
+            switch (movement.direction)
+            {
+                case MovementDirection.UP_RIGHT:
+                case MovementDirection.DOWN_RIGHT:
+                case MovementDirection.DOWN_LEFT:
+                case MovementDirection.UP_LEFT:
+                {
+                    movementSpeed /= sqrt(2.0f);
+                    break;
+                }
+            }
+            var movementVec = directions[(int) movement.direction];
+            movementVec.x = movementVec.x * movementSpeed + translation.Value.x;
+            movementVec.y = movementVec.y * movementSpeed + translation.Value.y;
+            translation.Value = new float3(movementVec.x, movementVec.y, 0);
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new MovementSystemJob();
+        
+        job.deltaTime = UnityEngine.Time.deltaTime;
+        
+        
+        
+        // Now that the job is set up, schedule it to be run. 
+        return job.Schedule(this, inputDependencies);
+    }
+}
