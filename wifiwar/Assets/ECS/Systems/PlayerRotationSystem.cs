aed1a49dac2cd04c9de8f02d159ed6e2ea7f7356
Aurimas Cernius <aurimasc@unity3d.com>
2019-06-25 20:02:55 +0200

    Refactor player rotation system

--- wifiwar/Assets/ECS/Systems/PlayerRotationSystem.cs
+++ wifiwar/Assets/ECS/Systems/PlayerRotationSystem.cs
@@ -10,40 +10,20 @@ using UnityEngine;
 
 public class RotationSystem_just : JobComponentSystem
 {
-    private Unity.Entities.EntityQuery Player_Query;
-    private float rotationAngle = 0.7854F;
-
-    protected override void OnCreateManager()
+    [BurstCompile]
+    struct PlayerRotationSystemJob : IJobForEach<Rotation, MovementComponent>
     {
-        Player_Query = GetEntityQuery(ComponentType.ReadWrite<Unity.Transforms.Rotation>(),
-            ComponentType.ReadWrite<Unity.Transforms.Translation>(),
-            ComponentType.ReadOnly<MovementComponent>());
+        public void Execute(ref Rotation rotation, [ReadOnly] ref MovementComponent movement)
+        {
+            float rotationAngle = 0.7854F;
+            rotation.Value = quaternion.AxisAngle(new float3(0F, 1F, 0F), rotationAngle * ((int)movement.direction));
+        }
     }
 
-    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
-        inputDeps.Complete();
-        {
-            var Player_QueryEntities = Player_Query.ToEntityArray(Allocator.TempJob);
-            var Player_QueryRotationArray = Player_Query.ToComponentDataArray<Unity.Transforms.Rotation>(Allocator.TempJob);
-            var Player_MovementSystemArray = Player_Query.ToComponentDataArray<MovementComponent>(Allocator.TempJob);
-
-            for (int Player_QueryIdx = 0; Player_QueryIdx < Player_QueryEntities.Length; Player_QueryIdx++)
-            {
-                var Cube_QueryEntity = Player_QueryEntities[Player_QueryIdx];
-                var Cube_QueryRotation = Player_QueryRotationArray[Player_QueryIdx];
-                var Player_MovementSystem = Player_MovementSystemArray[Player_QueryIdx];
-
-                Cube_QueryRotation.Value = quaternion.AxisAngle(new float3(0F, 1F, 0F), rotationAngle * (int)Player_MovementSystem.direction);
-                
-                EntityManager.SetComponentData<Unity.Transforms.Rotation>(Player_QueryEntities[Player_QueryIdx], Cube_QueryRotation);
-            }
-            
-            Player_QueryRotationArray.Dispose();
-            Player_QueryEntities.Dispose();
-            Player_MovementSystemArray.Dispose();
-        }
-
-        return inputDeps;
+        var job = new PlayerRotationSystemJob();
+        
+        return job.Schedule(this, inputDependencies);
     }
 }
