e0286100a8206bde179326d1b5ca87d0feb8b95a
Aurimas Cernius <aurimasc@unity3d.com>
2019-06-25 17:56:40 +0200

    Refactor to allow multiple input systems with similar base

---
+++ wifiwar/Assets/ECS/Systems/BaseInputSystem.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public abstract class BaseInputSystem : JobComponentSystem
+{
+    [BurstCompile]
+    struct MovementInputSystemJob : IJobForEach<MovementComponent>
+    {
+        public MovementDirection direction;
+        public float speed;
+
+        public void Execute(ref MovementComponent movement)
+        {
+            movement.direction = direction;
+            movement.speed = speed;
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new MovementInputSystemJob();
+        MovementDirection direction;
+        if (TryGetMovementDirection(out direction))
+        {
+            job.direction = direction;
+            job.speed = MovementSystem.MaxSpeed;
+        }
+        else
+        {
+            job.speed = 0;
+        }
+        return job.Schedule(this, inputDependencies);
+    }
+
+    protected abstract bool TryGetMovementDirection(out MovementDirection direction);
+}
--- wifiwar/Assets/ECS/Systems/KeyboardInputSystem.cs
+++ wifiwar/Assets/ECS/Systems/KeyboardInputSystem.cs
@@ -7,46 +7,9 @@ using Unity.Transforms;
 using UnityEngine;
 using static Unity.Mathematics.math;
 
-public class KeyboardInputSystem : JobComponentSystem
+public class KeyboardInputSystem : BaseInputSystem
 {
-    // This declares a new kind of job, which is a unit of work to do.
-    // The job is declared as an IJobForEach<Translation, Rotation>,
-    // meaning it will process all entities in the world that have both
-    // Translation and Rotation components. Change it to process the component
-    // types you want.
-    //
-    // The job is also tagged with the BurstCompile attribute, which means
-    // that the Burst compiler will optimize it for the best performance.
-    [BurstCompile]
-    struct KeyboardInputSystemJob : IJobForEach<MovementComponent>
-    {
-        public MovementDirection direction;
-        public float speed;
-
-        public void Execute(ref MovementComponent movement)
-        {
-            movement.direction = direction;
-            movement.speed = speed;
-        }
-    }
-    
-    protected override JobHandle OnUpdate(JobHandle inputDependencies)
-    {
-        var job = new KeyboardInputSystemJob();
-        MovementDirection direction;
-        if (TryGetMovementDirection(out direction))
-        {
-            job.direction = direction;
-            job.speed = MovementSystem.MaxSpeed;
-        }
-        else
-        {
-            job.speed = 0;
-        }
-        return job.Schedule(this, inputDependencies);
-    }
-    
-    bool TryGetMovementDirection(out MovementDirection direction)
+    protected override bool TryGetMovementDirection(out MovementDirection direction)
     {
         direction = MovementDirection.UP;
         
