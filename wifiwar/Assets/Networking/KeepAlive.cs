c73d2ec1aa191000508c96e576e020699646d447
Ryan Jones <ryanjo@RYANJONES-MBP.local>
2019-06-25 14:24:48 +0200

    Added a keep-alive script to the client scene.

---
+++ wifiwar/Assets/Networking/KeepAlive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ClientMessages;
+using UnityEngine;
+
+public class KeepAlive : MonoBehaviour
+{
+    public IEnumerator StartKeepAliveCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Debug.Log("Keep Alive Hello Sent ");
+            ClientMessageHello hello = new ClientMessageHello();
+            ServerConnection.Send(hello);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartKeepAliveCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
