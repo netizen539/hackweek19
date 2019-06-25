e12e4ea7824d69153ae9953609227e35a41ad393
DanLWallace <dlw.danwallace@gmail.com>
2019-06-25 13:06:22 +0200

    Physics and a Trigger Example
    Physics and a Trigger Example

---
+++ wifiwar/Assets/Spawner_Mono.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class Spawner_Mono : MonoBehaviour
+{
+    public GameObject Prefab;
+
+    void Start()
+    {
+        // Create entity prefab from the game object hierarchy once
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, World.Active);
+        var entityManager = World.Active.EntityManager;
+
+        var instance = entityManager.Instantiate(prefab);
+
+        var position = transform.TransformPoint(0.0f, 0.0f,0.0f);
+        entityManager.SetComponentData(instance, new Translation { Value = position });
+    }
+}
