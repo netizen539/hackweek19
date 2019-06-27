8c864303bfd297e61ecaca46a5dc3608cc1d64f1
DanLWallace <dlw.danwallace@gmail.com>
2019-06-27 17:59:56 +0200

    Bullets Now Hit Wall
    Bullets Now Hit Wall

---
+++ wifiwar/Assets/ECS/Components/Wall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Wall : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var data = new WallTag { };
+        dstManager.AddComponentData(entity, data);
+    }
+}
