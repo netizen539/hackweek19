805f13998ee65ec40350892084ff767b8a6bbace
LiterallyTheWorst <49045820+LiterallyTheWorst@users.noreply.github.com>
2019-06-27 17:02:29 +0300

    EDITOR ONLY: possible to have 2 players, 1 controlled with WASD other with ARROW keys.
    Shooting and shield is still same for both

--- wifiwar/Assets/ECS/Systems/KeyboardInputSystem.cs
+++ wifiwar/Assets/ECS/Systems/KeyboardInputSystem.cs
@@ -9,11 +9,19 @@ using static Unity.Mathematics.math;
 
 public class KeyboardInputSystem : BaseInputSystem
 {
+#if UNITY_EDITOR
+    protected override bool TryGetMovementDirectionAxis(out float2 playerDirectionAxis, out float2 player2DirectionAxis)
+#else
     protected override bool TryGetMovementDirectionAxis(out float2 playerDirectionAxis)
+#endif
     {
         float2 directionAxis = new float2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
         playerDirectionAxis = directionAxis;
+
+#if UNITY_EDITOR
+        float2 directionAxis2 = new float2(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1"));
+        player2DirectionAxis = directionAxis2;
+#endif
         return true;
     }
 
