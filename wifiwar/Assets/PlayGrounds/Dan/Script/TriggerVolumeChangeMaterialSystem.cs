5104ff30923db8e9fc24077ed7275a5c5909c426
DanLWallace <dlw.danwallace@gmail.com>
2019-06-27 11:37:50 +0200

    Power Up Stuff Test Scenes

--- wifiwar/Assets/PlayGrounds/Dan/Script/TriggerVolumeChangeMaterialSystem.cs
+++ wifiwar/Assets/PlayGrounds/Dan/Script/TriggerVolumeChangeMaterialSystem.cs
@@ -1,84 +1,84 @@
-﻿using Unity.Physics.Systems;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Rendering;
-using Unity.Burst;
+﻿//using Unity.Physics.Systems;
+//using Unity.Collections;
+//using Unity.Entities;
+//using Unity.Rendering;
+//using Unity.Burst;
 
-[UpdateBefore(typeof(BuildPhysicsWorld))]
-public class TriggerVolumeChangeMaterialSystem : ComponentSystem
-{
-    EndSimulationEntityCommandBufferSystem m_EntityCommandBufferSystem;
-    EntityQuery m_OverlappingGroup;
+//[UpdateBefore(typeof(BuildPhysicsWorld))]
+//public class TriggerVolumeChangeMaterialSystem : ComponentSystem
+//{
+//    EndSimulationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+//    EntityQuery m_OverlappingGroup;
 
-    protected override void OnCreate()
-    {
-        m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-        m_OverlappingGroup = GetEntityQuery(new EntityQueryDesc
-        {
-            All = new ComponentType[]
-            {
-                typeof(OverlappingTriggerVolume),
-                typeof(ChangeMaterialOverlappingTriggerVolume),
-            }
-        });
-    }
+//    protected override void OnCreate()
+//    {
+//        m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+//        m_OverlappingGroup = GetEntityQuery(new EntityQueryDesc
+//        {
+//            All = new ComponentType[]
+//            {
+//                typeof(OverlappingTriggerVolume),
+//                typeof(ChangeMaterialOverlappingTriggerVolume),
+//            }
+//        });
+//    }
 
-    [BurstCompile]
-    protected override void OnUpdate()
-    {
-        var overlappingComponents = GetComponentDataFromEntity<OverlappingTriggerVolume>();
+//    [BurstCompile]
+//    protected override void OnUpdate()
+//    {
+//        var overlappingComponents = GetComponentDataFromEntity<OverlappingTriggerVolume>();
 
-        using (var overlappingEntities = m_OverlappingGroup.ToEntityArray(Allocator.TempJob))
-        {
-            foreach (var entity in overlappingEntities)
-            {
-                var overlapComponent = overlappingComponents[entity];
-                if (overlapComponent.HasJustEntered)
-                {
-                    var volumeEntity = overlapComponent.VolumeEntity;
+//        using (var overlappingEntities = m_OverlappingGroup.ToEntityArray(Allocator.TempJob))
+//        {
+//            foreach (var entity in overlappingEntities)
+//            {
+//                var overlapComponent = overlappingComponents[entity];
+//                if (overlapComponent.HasJustEntered)
+//                {
+//                    var volumeEntity = overlapComponent.VolumeEntity;
 
-                    var volumeRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(volumeEntity);
-                    var overlappingRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(entity);
-                    overlappingRenderMesh.material = volumeRenderMesh.material;
+//                    var volumeRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(volumeEntity);
+//                    var overlappingRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(entity);
+//                    overlappingRenderMesh.material = volumeRenderMesh.material;
 
-                    PostUpdateCommands.SetSharedComponent<RenderMesh>(entity, overlappingRenderMesh);
-                }
-            }
-        }
-    }
+//                    PostUpdateCommands.SetSharedComponent<RenderMesh>(entity, overlappingRenderMesh);
+//                }
+//            }
+//        }
+//    }
 
 
-    ////[BurstCompile]
-    //struct ChangeMaterialJob : IJobForEachWithEntity<ExitedTriggerVolume>
-    //{
-    //    public EntityCommandBuffer CommandBuffer;
-    //    public EntityManager EntityManager;
+//    ////[BurstCompile]
+//    //struct ChangeMaterialJob : IJobForEachWithEntity<ExitedTriggerVolume>
+//    //{
+//    //    public EntityCommandBuffer CommandBuffer;
+//    //    public EntityManager EntityManager;
 
-    //    public void Execute(Entity entity, int index, ref ExitedTriggerVolume exitedTrigger)
-    //    {
-    //        if (exitedTrigger.volumeId < 10)
-    //        {
-    //            var volumeEntity = exitedTrigger.volumeEntity;
+//    //    public void Execute(Entity entity, int index, ref ExitedTriggerVolume exitedTrigger)
+//    //    {
+//    //        if (exitedTrigger.volumeId < 10)
+//    //        {
+//    //            var volumeEntity = exitedTrigger.volumeEntity;
 
-    //            var volumeRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(volumeEntity);
-    //            var overlappingRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(entity);
-    //            overlappingRenderMesh.material = volumeRenderMesh.material;
+//    //            var volumeRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(volumeEntity);
+//    //            var overlappingRenderMesh = EntityManager.GetSharedComponentData<RenderMesh>(entity);
+//    //            overlappingRenderMesh.material = volumeRenderMesh.material;
 
-    //            CommandBuffer.SetSharedComponent<RenderMesh>(entity, overlappingRenderMesh);
-    //        }
-    //    }
-    //}
-    
-    //protected override JobHandle OnUpdate(JobHandle inputDeps)
-    //{
-    //    var job = new ChangeMaterialJob
-    //    {
-    //        CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer(),
-    //        EntityManager = EntityManager,
-    //    }.ScheduleSingle(this, inputDeps);
+//    //            CommandBuffer.SetSharedComponent<RenderMesh>(entity, overlappingRenderMesh);
+//    //        }
+//    //    }
+//    //}
 
-    //    m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+//    //protected override JobHandle OnUpdate(JobHandle inputDeps)
+//    //{
+//    //    var job = new ChangeMaterialJob
+//    //    {
+//    //        CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer(),
+//    //        EntityManager = EntityManager,
+//    //    }.ScheduleSingle(this, inputDeps);
 
-    //    return job;
-    //}
-}
+//    //    m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+
+//    //    return job;
+//    //}
+//}
